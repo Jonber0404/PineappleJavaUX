3a58142d4e661e378ed8e6364be464f6581a98c7
Ludvig Ward <ludvig@upward.se>
2024-04-19 20:11:11 +0200

    Search query for axes, with simple vue web app

---
+++ script.js
@@ -0,0 +1,28 @@
+const app = {
+    data() {
+        return {
+            objekt: {},
+            objektBild: ""
+        }
+    },
+    methods: {
+        getObjekt() {
+            const searchText = "yxa"
+            // Man kan ersätta totalAmount med elementet "totalHits" från K-samsök
+            const totalAmount = 15528;
+
+            let randomObjekt = Math.floor((Math.random() * totalAmount) + 1)
+            fetch(`https://kulturarvsdata.se/ksamsok/api?method=search&hitsPerPage=1&startRecord=${randomObjekt}&query=text=${searchText}+AND+thumbnailExists=j`,
+                {headers: {'Accept': 'application/json'}})
+                .then(resp => resp.json())
+                .then(json => this.objekt = json)
+
+            // fixa med async och await här
+            this.objektBild = this.objekt.result.records[0].record['@graph'].find(obj => obj.thumbnailSource).thumbnailSource
+        }
+    },
+    mounted() {
+        this.getObjekt()
+    }
+}
+Vue.createApp(app).mount("#app")
