cdd038f6f87f98e13a11d94b25e012f0673fa990
Ludvig Ward <ludvig@upward.se>
2024-04-20 13:10:15 +0200

    Added date query

--- script.js
+++ script.js
@@ -2,23 +2,25 @@ const app = {
     data() {
         return {
             objekt: {},
-            objektBild: ""
+            objektBild: "",
+            objektDatum: ""
         }
     },
     methods: {
         getObjekt() {
             const searchText = "yxa"
             // Man kan ersätta totalAmount med elementet "totalHits" från K-samsök
-            const totalAmount = 15528;
+            const totalAmount = 6407;
 
             let randomObjekt = Math.floor((Math.random() * totalAmount) + 1)
-            fetch(`https://kulturarvsdata.se/ksamsok/api?method=search&hitsPerPage=1&startRecord=${randomObjekt}&query=text=${searchText}+AND+thumbnailExists=j`,
+            fetch(`https://kulturarvsdata.se/ksamsok/api?method=search&hitsPerPage=1&startRecord=${randomObjekt}&query=text=${searchText}+AND+thumbnailExists=j+AND+timeInfoExists=j`,
                 {headers: {'Accept': 'application/json'}})
                 .then(resp => resp.json())
                 .then(json => this.objekt = json)
 
             // fixa med async och await här
             this.objektBild = this.objekt.result.records[0].record['@graph'].find(obj => obj.thumbnailSource).thumbnailSource
+            this.objektDatum = this.objekt.result.records[0].record['@graph'].find(obj => obj.fromTime).fromTime
         }
     },
     mounted() {
