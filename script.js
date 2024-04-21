db590419b4c930258d41ccd627c359ae11379c83
Ludvig Ward <ludvig@upward.se>
2024-04-21 19:38:20 +0200

    Added async/await and loop to check if results are correctly formatted

--- script.js
+++ script.js
@@ -7,20 +7,28 @@ const app = {
         }
     },
     methods: {
-        getObjekt() {
+        async getObjekt() {
             const searchText = "yxa"
             // Man kan ersätta totalAmount med elementet "totalHits" från K-samsök
             const totalAmount = 6407;
 
             let randomObjekt = Math.floor((Math.random() * totalAmount) + 1)
-            fetch(`https://kulturarvsdata.se/ksamsok/api?method=search&hitsPerPage=1&startRecord=${randomObjekt}&query=text=${searchText}+AND+thumbnailExists=j+AND+timeInfoExists=j`,
-                {headers: {'Accept': 'application/json'}})
-                .then(resp => resp.json())
-                .then(json => this.objekt = json)
+            try {
+                // do-while loop för att se till att svaret man får faktiskt följer den vanliga api-objektstrukturen.
+                // Förvånansvärt många objekt i K-samsök har felaktig formatering...
+                do {
+                    const response = await fetch(`https://kulturarvsdata.se/ksamsok/api?method=search&hitsPerPage=1&startRecord=${randomObjekt}&query=text=${searchText}+AND+thumbnailExists=j+AND+timeInfoExists=j`, {
+                        headers: {'Accept': 'application/json'}
+                    });
+                    const json = await response.json();
+                    this.objekt = json;
+                    this.objektBild = this.objekt.result.records[0].record['@graph'].find(obj => obj.thumbnailSource).thumbnailSource;
+                    this.objektDatum = this.objekt.result.records[0].record['@graph'].find(obj => obj.fromTime).fromTime;
+                } while (Number.isNaN(this.objektDatum))
 
-            // fixa med async och await här
-            this.objektBild = this.objekt.result.records[0].record['@graph'].find(obj => obj.thumbnailSource).thumbnailSource
-            this.objektDatum = this.objekt.result.records[0].record['@graph'].find(obj => obj.fromTime).fromTime
+            } catch (error) {
+                console.error('Error fetching data:', error);
+            }
         }
     },
     mounted() {
