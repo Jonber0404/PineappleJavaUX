c947d0d00b54a0efdeb474e672cde877c4d264cd
Ludvig Ward <ludvig@upward.se>
2024-04-22 12:04:21 +0200

    Added stuff

--- script.js
+++ script.js
@@ -8,21 +8,33 @@ const app = {
     },
     methods: {
         async getObjekt() {
-            const searchText = "yxa"
+            const decadeStart = 1980;
+            const decadeEnd = decadeStart + 9;
             // Man kan ersätta totalAmount med elementet "totalHits" från K-samsök
+
             const totalAmount = 6407;
 
             let randomObjekt = Math.floor((Math.random() * totalAmount) + 1)
             try {
-                const response = await fetch(`https://kulturarvsdata.se/ksamsok/api?method=search&hitsPerPage=1&startRecord=${randomObjekt}&query=text=${searchText}+AND+thumbnailExists=j+AND+timeInfoExists=j`, {
+                this.objektDatum = "";
+
+                const response = await fetch(`https://kulturarvsdata.se/ksamsok/api?method=search&hitsPerPage=1&startRecord=${randomObjekt}&query=create_fromTime>=${decadeStart}+AND+create_fromTime<=${decadeEnd}+AND+itemType=foto+AND+thumbnailExists=j+AND+timeInfoExists=j`, {
                     headers: {'Accept': 'application/json'}
                 });
-                const json = await response.json();
+                const json = await response.json()
                 this.objekt = json;
-                this.objektBild = this.objekt.result.records[0].record['@graph'].find(obj => obj.thumbnailSource).thumbnailSource;
-                this.objektDatum = this.objekt.result.records[0].record['@graph'].find(obj => obj.fromTime).fromTime;
+
+                const currentRecord = this.objekt.result.records[0].record['@graph']
+                this.objektBild = currentRecord.find(obj => obj.lowresSource).lowresSource
+                for (obj of currentRecord) {
+                    if (Object.keys(obj).includes("contextLabel")) {
+                        if (Object.values(obj.contextLabel).includes("Fotograferad")) {
+                            this.objektDatum = obj.fromTime
+                        }
+                    }
+                }
             } catch (error) {
-                console.error('Error fetching data:', error);
+                console.error('Error fetching data:', error)
             }
         }
     },
