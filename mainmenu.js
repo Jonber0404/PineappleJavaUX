bd6495131ebe6ded8b08f6cfa9c22699909c2477
Jonber0404 <jonber0404@skola.goteborg.se>
2024-04-30 11:33:03 +0200

    Mobile first for main menu and player selection

--- mainmenu.js
+++ mainmenu.js
@@ -4,19 +4,19 @@ const vueApp = Vue.createApp(app)
 
 const playbutton ={
     name: "PlayButton",
-    template: `<a href="playerselection.html"><button class='playbutton'>Spela</button></a>`
+    template: `<a href="playerselection.html"><button class='playbutton startmenubutton'>Spela</button></a>`
 }
 vueApp.component("play-button", playbutton)
 
 const scoreboardbutton ={
     name: "ScoreboardButton",
-    template: `<a href="scoreboard.html"><button class='scoreboardbutton'>Scoreboard</button></a>`
+    template: `<a href="scoreboard.html"><button class='scoreboardbutton startmenubutton'>Scoreboard</button></a>`
 }
 vueApp.component("scoreboard-button", scoreboardbutton)
 
 const gamerulesbutton ={
     name: "GamerulesButton",
-    template: `<a href="gamerules.html"><button class='gamerulesbutton'>Spelregler</button></a>`
+    template: `<a href="gamerules.html"><button class='gamerulesbutton startmenubutton'>Spelregler</button></a>`
 }
 vueApp.component("gamerules-button", gamerulesbutton)
 
